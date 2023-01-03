442ae7cfebe8f56d9bcf0f64d4623752c2f73726
Niten Nashiki <niten.nashiki@lightz-inc.com>
2023-01-03 12:14:47 +0900

    型を追加

--- app/routers/super/tenants.py
+++ app/routers/super/tenants.py
@@ -4,7 +4,7 @@ from app.database import SuperSession, get_super_db
 from app.dependencies import get_my_rights
 from app.exceptions import NoRightsException
 from app.models import Right
-from app.schemas.tenant import CreateTenantRequestBody, ReadManyTenantsRequestQueryParam
+from app.schemas.tenant import CreateTenantRequestBody, ReadManyTenantsRequestQueryParam, TenantResponse
 from app.services.authorization import has_any_rights
 from app.usecases import tenant
 
@@ -19,13 +19,13 @@ def create_tenant(
     create_tenants_request_body: CreateTenantRequestBody,
     session: SuperSession = Depends(get_super_db),
     my_rights: list[Right] = Depends(get_my_rights),
-):
+) -> None:
     if not has_any_rights(my_rights=my_rights, require_rights=[Right(name="create.tenant")]):
         raise NoRightsException
-    new = tenant.create_tenant(
+    _ = tenant.create_tenant(
         session, name=create_tenants_request_body.name, service_plan_id=create_tenants_request_body.service_plan_id
     )
-    return new
+    return None
 
 
 @router.get("/")
@@ -33,7 +33,7 @@ def read_many_tenants(
     session: SuperSession = Depends(get_super_db),
     query_param: ReadManyTenantsRequestQueryParam = Depends(),
     my_rights: list[Right] = Depends(get_my_rights),
-):
+) -> list[TenantResponse]:
     if not has_any_rights(my_rights=my_rights, require_rights=[Right(name="read.tenant")]):
         return []
     return tenant.read_many_tenants(session, name=query_param.name)
